12546ab6c949ba94ef068bcb2de8af7997475584
mnjg1 <mnjg1@10.8.0.2>
2020-03-27 23:25:48 +0100

    Inital Commit

---
+++ src/me/mnjg123/ProxySocke/TSVerify/main/PSVerify.java
@@ -0,0 +1,35 @@
+/**
+ * @author mnjg123
+ *
+ * @license Licensed to ProxySocke.NET and their Management, unauthorized replication is prohibited!
+ *
+ * Copyright 27 Mar 2020 mnjg123 ( including mnjg123.de, mnjg123.eu, devdb.eu ), ProxySocke to Present
+ * All Rights Reserved.
+ */
+package me.mnjg123.ProxySocke.TSVerify.main;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ * 
+ * @author mnjg123
+ *
+ */
+public class PSVerify extends Plugin {
+	
+	@Override
+	public void onEnable() {
+		
+		
+		super.onEnable();
+	}
+
+	
+	@Override
+	public void onDisable() {
+		
+		
+		super.onDisable();
+	}
+	
+}
